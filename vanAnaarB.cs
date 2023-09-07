1e8c7349aeee86e3a318dac5268d9c7bab4589af
delysha <34533@ma-web.nl>
2023-09-07 11:50:26 +0200

    Added moving platform

---
+++ vanAnaarB.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vanAnaarB : MonoBehaviour
+{
+    public float speed = 2;
+    void Update()
+    {
+        transform.position = new Vector3 
+        (Mathf.PingPong(Time.time*speed, 4), transform.position.y, transform.position.z);
+    }
+
+
+}
